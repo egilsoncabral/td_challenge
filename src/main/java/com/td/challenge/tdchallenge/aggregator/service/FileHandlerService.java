bd8b99747ac4215269ee29e3d049ac1852d65823
Egilson Cabral <egilson.cabral@timwe.com>
2021-02-02 01:58:24 +0000

    Docker configs

--- src/main/java/com/td/challenge/tdchallenge/aggregator/service/FileHandlerService.java
+++ src/main/java/com/td/challenge/tdchallenge/aggregator/service/FileHandlerService.java
@@ -5,11 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,16 +20,18 @@ public class FileHandlerService {
     Set<String> files;
 
     @PostConstruct
-    public void loadFile() throws IOException, URISyntaxException {
+    public void loadFile() {
         if (files == null){
             files = readFile(filePath);
         }
     }
 
-    private Set<String> readFile(String filePath) throws IOException, URISyntaxException {
-        Path path = Paths.get(getClass().getClassLoader()
-                .getResource(filePath).toURI());
-        return Files.lines(path).collect(Collectors.toSet());
+    private Set<String> readFile(String filePath) {
+        InputStream in = getClass().getResourceAsStream(filePath);
+        return new Scanner(in)
+                .useDelimiter("\n")
+                .tokens()
+                .collect(Collectors.toSet());
     }
 
     public Set<String> getFiles() {
--- src/test/java/com/td/challenge/tdchallenge/aggregator/service/FileHandleServiceTest.java
+++ src/test/java/com/td/challenge/tdchallenge/aggregator/service/FileHandleServiceTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,13 +15,13 @@ public class FileHandleServiceTest {
     private FileHandlerService fileHandlerService;
 
     @BeforeEach
-    void setUp() throws IOException, URISyntaxException {
-        fileHandlerService.setFilePath("prefixes.txt");
+    void setUp() {
+        fileHandlerService.setFilePath("/prefixes.txt");
         fileHandlerService.loadFile();
     }
 
     @Test
-    void when_loadFile_then_return_prefixes_list() throws IOException, URISyntaxException {
+    void when_loadFile_then_return_prefixes_list() {
 
         // Act
         var actual = fileHandlerService.getFiles();
